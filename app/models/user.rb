894e65d62575499e925c65a23b5be892d7561ec7
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-12-11 00:13:39 -0500

    Update database

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
+  enum role: {user: 0, admin: 1}
 end
