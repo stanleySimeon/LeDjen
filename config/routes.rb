69ed92175cb558a7aace21034c8751114dbd50cc
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-12-21 00:51:23 -0500

    Update database

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  # devise_for :users
-  root 'home#index'
-  resources :comments do
-    resources :comments, only: [:create, :show, :update, :destroy]
+  devise_for :users
+  root 'users#index'
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
+      get '/comments', to: 'comments#new'
+      resources :likes, only: [:new, :create]
+      get '/likes', to: 'likes#new'
+    end
   end
   resources :contacts, only: [:new, :create]
   get '/contacts', to: 'contacts#new'
